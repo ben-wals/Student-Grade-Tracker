48605a8c72f1dbfaae453da1a41b898c44780409
Ben <thescientist771.mail@gmail.com>
2024-02-14 19:46:54 +0000

    v0.0.3
    Frame Layouts

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # imports the python module tkinter to be reference as "tk" throughout the program
-import tkinter as tk
+from tkinter import *
 
 # creates a tkinter window to be referenced as "sgt" throughout the program
-sgt = tk.Tk()
+sgt = Tk()
 
-# outlines some basic styling for the window, title and icon
+# outlines some basic styling for the window, title and icon and size
 sgt.title("Student Grade Tracker") 
 sgt.iconbitmap("./img/iceburg.ico")
+sgt.configure(bg="#FAF9F6")
+sgt.maxsize(630, 420)
+sgt.minsize(630, 420)
+
+sidebar = Frame(sgt, width=200, height=400)
+sidebar.grid(row=0, column=0, padx=(10, 5), pady=10)
+
+gradeViewer = Frame(sgt, width=400, height=400)
+gradeViewer.grid(row=0, column=1, padx=(5, 10), pady=10)
 
 # starts the main loop of the window
 sgt.mainloop()
