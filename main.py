95596d26340bbff3f8d6be8838f0cb470d3ac98f
Ben <thescientist771.mail@gmail.com>
2024-02-14 19:05:13 +0000

    v0.0.2
    Code Comments for v0.0.1

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# imports the python module tkinter to be reference as "tk" throughout the program
 import tkinter as tk
 
+# creates a tkinter window to be referenced as "sgt" throughout the program
 sgt = tk.Tk()
+
+# outlines some basic styling for the window, title and icon
 sgt.title("Student Grade Tracker") 
 sgt.iconbitmap("./img/iceburg.ico")
 
+# starts the main loop of the window
 sgt.mainloop()
