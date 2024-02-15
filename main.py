4ef1e1ad639f912aa71ba785413900766fabc76a
Ben <thescientist771.mail@gmail.com>
2024-02-15 12:41:20 +0000

    v0.0.5
    Loads a list box of classes from data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # imports the python module tkinter to be reference as "tk" throughout the program
 from tkinter import *
+import os
 
 # creates a tkinter window to be referenced as "sgt" throughout the program
 sgt = Tk()
@@ -13,6 +14,18 @@ sgt.minsize(630, 420)
 
 sidebar = Frame(sgt, width=200, height=400)
 sidebar.grid(row=0, column=0, padx=(10, 5), pady=10)
+
+listbox = Listbox(sidebar, width=33, height=22)
+listbox.grid(row=2, column=0)
+
+text = Label(sidebar, text="Classes", font=("Helvetica", 9))
+text.grid(row=1, column=0)
+
+text = Label(sidebar, text="Student Grade Tracker", font=("Helvetica", 12))
+text.grid(row=0, column=0)
+
+for values in os.listdir("./data"):
+    listbox.insert(END, values)
 
 gradeViewer = Frame(sgt, width=400, height=400)
 gradeViewer.grid(row=0, column=1, padx=(5, 10), pady=10)
