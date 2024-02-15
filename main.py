ac2a21456fc37bf24bb9fe97667933ae8777c222
Ben <thescientist771.mail@gmail.com>
2024-02-15 23:07:28 +0000

    v0.0.8
    Added some test data and begun work on the gradeViewer which is dynamic per the selection of the class from the listbox

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # imports the python module tkinter to be reference as "tk" throughout the program
 from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk 
 import os
 
 # creates a tkinter window to be referenced as "sgt" throughout the program
@@ -12,23 +14,58 @@ sgt.configure(bg="#FAF9F6")
 sgt.maxsize(630, 420)
 sgt.minsize(630, 420)
 
+def addClass():
+    messagebox.showinfo("Message", "Click Okay to Proceed")
+
+def selected_item():
+    for i in listbox.curselection():
+        return listbox.get(i)
+
+def loadClass():
+    className = selected_item()
+    frameTitle.set(className)
+    tabControl = ttk.Notebook(gradeViewer, width=380)
+
+    average = ttk.Frame(tabControl) 
+    hsl = ttk.Frame(tabControl)
+    overview = ttk.Frame(tabControl) 
+
+    tabControl.add(average, text ='Averages') 
+    tabControl.add(hsl, text ='Highest / Lowest')
+    tabControl.add(overview, text ='Overview') 
+    tabControl.grid(row=1, column=0, padx=5)
+
 sidebar = Frame(sgt, width=200, height=400)
 sidebar.grid(row=0, column=0, padx=(10, 5), pady=10)
-
-listbox = Listbox(sidebar, width=33, height=22)
-listbox.grid(row=2, column=0)
-
-text = Label(sidebar, text="Classes", font=("Helvetica", 9))
-text.grid(row=1, column=0)
+sidebar.grid_propagate(False)
 
 text = Label(sidebar, text="Student Grade Tracker", font=("Helvetica", 12))
-text.grid(row=0, column=0)
+text.grid(row=0, column=0, columnspan=2)
+
+text = Label(sidebar, text="Classes", font=("Helvetica", 9))
+text.grid(row=1, column=0, columnspan=2)
+
+listbox = Listbox(sidebar, width=31, height=19)
+listbox.grid(row=2, column=0, padx=5, columnspan=2)
 
 for values in os.listdir("./data"):
     listbox.insert(END, values)
 
+text = Button(sidebar, text="Add Class", font=("Helvetica", 9), command=addClass)
+text.grid(row=3, column=0, pady=5)
+
+text = Button(sidebar, text="Load Class", font=("Helvetica", 9), command=loadClass)
+text.grid(row=3, column=1, pady=5)
+
 gradeViewer = Frame(sgt, width=400, height=400)
 gradeViewer.grid(row=0, column=1, padx=(5, 10), pady=10)
+gradeViewer.grid_propagate(False)
+
+frameTitle = StringVar()
+frameTitle.set("Please Select A Class From The List...")
+
+classText = Label(gradeViewer, textvariable=frameTitle, font=("Helvetica", 12))
+classText.grid(row=0, column=0)
 
 # starts the main loop of the window
 sgt.mainloop()
